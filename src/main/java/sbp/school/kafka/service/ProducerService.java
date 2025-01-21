636668698774de43261bf0fe0078b9c439161586
IlyaVostrukhin <i.s.vostrukhin@yandex.ru>
2025-01-21 13:09:08 +0700

    vostrukhin_kafka_1 fixed comments

--- src/main/java/sbp/school/kafka/service/ProducerService.java
+++ src/main/java/sbp/school/kafka/service/ProducerService.java
@@ -3,6 +3,7 @@ package sbp.school.kafka.service;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import sbp.school.kafka.config.KafkaConfig;
 import sbp.school.kafka.config.PropertiesReader;
 import sbp.school.kafka.dto.TransactionDto;
@@ -11,30 +12,32 @@ import sbp.school.kafka.dto.TransactionDto;
 public class ProducerService {
 
     private final KafkaProducer<String, TransactionDto> producer;
+    private final String topicName = PropertiesReader
+            .readProperties("application.properties")
+            .getProperty("transaction.topic");
 
     public ProducerService() {
         producer = KafkaConfig.getTransactionProducer();
     }
 
     public void sendTransaction(TransactionDto transaction) {
-        String topicName = PropertiesReader
-                .readProperties("application.properties")
-                .getProperty("transaction.topic");
-
-        producer.send(new ProducerRecord<>(topicName, transaction.getOperationType().name(), transaction),
-                ((recordMetadata, e) -> {
-                    if (e != null) {
-                        log.error("Ошибка отправки сообщения! Offset: {}, Partition: {}, Error: {}",
-                                recordMetadata.offset(),
-                                recordMetadata.partition(),
-                                e.getMessage()
-                        );
-                    } else {
-                        log.trace("Сообщение успешно отправлено: {}", transaction);
-                    }
-                })
+        producer.send(
+                new ProducerRecord<>(topicName, transaction.getOperationType().name(), transaction),
+                (recordMetadata, e) -> callback(recordMetadata, e, transaction)
         );
 
         producer.flush();
+    }
+
+    private void callback(RecordMetadata recordMetadata, Exception e, TransactionDto transaction) {
+        if (e != null) {
+            log.error("Ошибка отправки сообщения! Offset: {}, Partition: {}, Error: {}",
+                    recordMetadata.offset(),
+                    recordMetadata.partition(),
+                    e.getMessage()
+            );
+        } else {
+            log.trace("Сообщение успешно отправлено: {}", transaction);
+        }
     }
 }
